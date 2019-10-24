b0d8c360ad6999022d909c8256f4100e2375fa7a
757265748 <757265748@qq.com>
2019-10-24 17:46:54 +0800

    券后价格小数处理

---
+++ pages/common/to-fixed.vue
@@ -0,0 +1,25 @@
+<template>
+	<view>{{quanhoujia}}</view>
+</template>
+
+<script>
+	export default{
+		props:{
+			priceY:{
+				type:Number
+			},
+			youhuiquan:{
+				type:Number
+			}
+		},
+		computed:{
+			quanhoujia(){
+				console.log(this.priceY-this.youhuiquan);
+				return (this.priceY-this.youhuiquan).toFixed(1);
+			}
+		}
+	}
+</script>
+
+<style>
+</style>
